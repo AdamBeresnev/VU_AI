44a0e71e20f5ded32a68a71dc46bb1c5b3a4fc9d
AdamBeresnev <58663862+AdamBeresnev@users.noreply.github.com>
2023-11-13 16:34:02 +0200

    parasyta lentos klase

--- ZirguMarsrutai/zm.cpp
+++ ZirguMarsrutai/zm.cpp
@@ -7,7 +7,50 @@ struct zirgoKoordinates {
     int y;
 };
 
-int n;
+class lentosKlase {
+    private:
+    bool inicializuota;
+    int* lentosMasyvas;
+
+    void inicializacija(){
+        if (n == 0) { cout << "n negali buti lygus 0"; return; }
+        int laikinaLenta[n*n];
+        for(int i = 0; i < n*n; i++){
+            laikinaLenta[i] = 0;
+        }
+        lentosMasyvas = laikinaLenta;
+        inicializuota = true;
+    }
+    public:
+    int n = 0;
+    int ejimas = 1;
+
+    lentosKlase()
+    {
+        inicializuota = false;
+    }
+
+    int gautiReiksme(int x, int y){
+        if (!inicializuota) { inicializacija(); }
+        return lentosMasyvas[(x - 1) * n + (y - 1)];
+    }
+
+    void padarytEjima(int x, int y){
+        if (!inicializuota) { inicializacija(); }
+        lentosMasyvas[(x - 1) * n + (y - 1)] = ejimas;
+        ejimas++;
+    }
+
+    void grizti(int naujasEjimas){
+        for(int i = 0; i < n*n; i++){
+            if (lentosMasyvas[i] >= naujasEjimas) {
+                lentosMasyvas[i] = 0;
+            }
+        }
+    }
+};
+
+lentosKlase lenta;
 struct zirgoKoordinates zirgas;
 
 zirgoKoordinates taisykle1() { struct zirgoKoordinates ateitiesPozicija; ateitiesPozicija.x = zirgas.x + 2; ateitiesPozicija.y = zirgas.y + 1; return ateitiesPozicija;};
@@ -22,19 +65,20 @@ zirgoKoordinates taisykle8() { struct zirgoKoordinates ateitiesPozicija; ateitie
 void argumentuGavimas();
 
 int taisykliuTikrinimas();
-int taisykliuTikrinimas(int cikloPradzia);
+int taisykliuTikrinimas(int);
 
-bool tinkamaPozicija(zirgoKoordinates ateitiesPozicija);
+bool tinkamaPozicija(zirgoKoordinates);
 
 int main()
 {
     argumentuGavimas();
-    int cikloPozicija = taisykliuTikrinimas();
-    cout << cikloPozicija << zirgas.x << zirgas.y;
+    lenta.padarytEjima(1,1);
+    lenta.padarytEjima(1, 2);
+    cout << lenta.gautiReiksme(1, 2);
 }
 
 void argumentuGavimas(){
-    n = 5;
+    lenta.n = 5;
     zirgas = {1, 1};
 }
 
@@ -98,7 +142,7 @@ int taisykliuTikrinimas(int cikloPradzia){
 }
 
 bool tinkamaPozicija(zirgoKoordinates ateitiesPozicija){
-    if (ateitiesPozicija.x < 1 || ateitiesPozicija.y < 1 || ateitiesPozicija.x > n || ateitiesPozicija.y > n){
+    if (ateitiesPozicija.x < 1 || ateitiesPozicija.y < 1 || ateitiesPozicija.x > lenta.n || ateitiesPozicija.y > lenta.n){
         return false;
     }
     //reikia patikrint ar neuzejo ant liestos vietos
